63af0d692c971fbcae00687ecdecb3655abb4781
Kilderson Sena <dersonsena@gmail.com>
2021-09-18 01:28:38 -0300

    saving url and redirecting user

--- config/dic.php
+++ config/dic.php
@@ -1,8 +1,6 @@
 <?php
 
 use DI\ContainerBuilder;
-use Odan\Session\SessionInterface;
-use Slim\Flash\Messages;
 use Slim\Views\Twig;
 
 $containerBuilder = new ContainerBuilder();
@@ -22,9 +20,29 @@ $container->set('config', function() {
 
 $container->set('view', function () use ($container) {
     $twigConfig = $container->get('config')['twig'];
-    $flash = $container->get(Messages::class);
-    $session = $container->get(SessionInterface::class);
+    // $flash = $container->get(Messages::class);
+    // $session = $container->get(SessionInterface::class);
     $twig = Twig::create($twigConfig['templatePath'], ['cache' => $twigConfig['cachePath']]);
     $twig->getEnvironment()->addGlobal('baseUrl', $container->get('config')['baseUrl']);
     return $twig;
+});
+
+$container->set('db', function () use ($container) {
+    $dbConfig = $container->get('config')['database'];
+    try {
+        return new PDO(
+            sprintf('mysql:host=%s;dbname=%s', $dbConfig['host'], $dbConfig['dbname']),
+            $dbConfig['username'],
+            $dbConfig['password'],
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ]
+        );
+
+    } catch (PDOException $e) {
+        print "Database Error: " . $e->getMessage();
+        die;
+    }
 });
--- config/routes.php
+++ config/routes.php
@@ -4,27 +4,89 @@ declare(strict_types=1);
 
 use Psr\Http\Message\RequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use Ramsey\Uuid\Uuid;
 use Slim\App;
 use Slim\Routing\RouteCollectorProxy;
 
 return function (App $app) {
-    $app->get('/', function ($request, $response, $args) {
+    $app->get('/', function (Request $request, Response $response, array $args) {
         return $this->get('view')->render($response, 'index.html.twig', []);
-    })->setName('profile');
+    });
+
+    $app->get('/{path}', function (Request $request, Response $response, array $args) {
+        /** @var PDO $db */
+        $db = $this->get('db');
+        $stmt = $db->prepare("SELECT `id`, `long_url` FROM `urls` WHERE `short_url_path` = :path");
+        $stmt->execute(['path' => $args['path']]);
+        $row = $stmt->fetch();
+
+        /*if (!$row) {
+            //
+        }*/
+
+        $sql = "INSERT INTO `urls_logs` (`id`, `uuid`, `url_id`, `created_at`, `meta`) VALUES (:id, :uuid, :url_id, :created_at, :meta)";
+        $stmt = $db->prepare($sql);
+
+        $uuid = Uuid::uuid4();
+        $createdAt = new DateTimeImmutable();
+
+        $stmt->execute([
+            'id' => $uuid->getBytes(),
+            'uuid' => $uuid->toString(),
+            'url_id' => $row['id'],
+            'created_at' => $createdAt->format('Y-m-d H:i:s'),
+            'meta' => json_encode([
+                'REMOTE_ADDR' => filter_input(INPUT_SERVER, 'REMOTE_ADDR'),
+                'REMOTE_PORT' => filter_input(INPUT_SERVER, 'REMOTE_PORT'),
+                'SERVER_NAME' => filter_input(INPUT_SERVER, 'SERVER_NAME'),
+                'REQUEST_URI' => filter_input(INPUT_SERVER, 'REQUEST_URI'),
+                'HTTP_HOST' => filter_input(INPUT_SERVER, 'HTTP_HOST'),
+                'HTTP_USER_AGENT' => filter_input(INPUT_SERVER, 'HTTP_USER_AGENT'),
+            ]),
+        ]);
+
+        $newResponse = $response->withHeader('Location', $row['long_url'])
+            ->withStatus(302);
+
+        return $newResponse;
+    });
 
     $app->group('/api/public', function (RouteCollectorProxy $group) {
         $group->post('/shorten', function (Request $request, Response $response, array $args) {
+            /** @var PDO $db */
+            $db = $this->get('db');
+
+            $sql = "
+                INSERT INTO `urls` (`id`, `uuid`, `long_url`, `short_url_path`, `created_at`)
+                VALUES (:id, :uuid, :long_url, :short_url_path, :created_at)
+            ";
+            $stmt = $db->prepare($sql);
+
+            $contents = $request->getParsedBody();
+            $shortUrlPath = substr(sha1(uniqid((string)rand(), true)), 0, 10);
+            $uuid = Uuid::uuid4();
+            $createdAt = new DateTimeImmutable();
+
+            $stmt->execute([
+                'id' => $uuid->getBytes(),
+                'uuid' => $uuid->toString(),
+                'long_url' => $contents['huge_url'],
+                'short_url_path' => $shortUrlPath,
+                'created_at' => $createdAt->format('Y-m-d H:i:s')
+            ]);
+
             $newResponse = $response->withHeader('Content-type', 'application/json');
             $newResponse->withHeader('Content-type', 'application/json');
             $newResponse->withStatus(200);
             $newResponse->getBody()->write(json_encode([
                 'status' => 'success',
                 'data' => [
-                    'huge' => 'https://www.devmedia.com.br/ajax-com-jquery-trabalhando-com-requisicoes-assincronas/37141',
-                    'shortened' => 'https://smll.sh/abc123efg',
-                    'created_at' => (new DateTimeImmutable())->format(DateTimeInterface::ATOM)
+                    'huge' => $contents['huge_url'],
+                    'shortened' => 'https://smll.sh/' . $shortUrlPath,
+                    'created_at' => $createdAt->format(DateTimeInterface::ATOM)
                 ]
             ]));
+
             return $newResponse;
         });
     });
